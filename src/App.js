4c69d70441a6f744b9f9f1868a5e3107e02e5cfe
florenciasilva <hensgrej@gmail.com>
2020-01-19 20:49:12 -0600

    Add strict to every route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import UserAuth from './Components/Views/UserAuth';
 import Home from './Components/Views/Home';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
-  return (
 
+  return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={UserAuth} />
-        <Route path="/home" exact component={Home} />
+        <Route path="/" exact strict component={UserAuth} />
+        <Route path="/home" exact strict component={Home} protected />
       </Switch>
     </BrowserRouter>
   );
