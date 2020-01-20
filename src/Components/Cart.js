89374dc09466fc1aff8c58c8014ec866ee171dd6
florenciasilva <hensgrej@gmail.com>
2020-01-19 21:17:36 -0600

    Show total on cart component

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,21 +1,28 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Cart = (props) => {
-
-    if(props.products.length !== 0) {
-        return props.products.map((product, i) => {
+        if(props.products.length !== 0) {
+            let totalCart = 0;
+            const productList = props.products.map((product, i) => {
+                totalCart = totalCart + parseInt(product.price)
+                return (
+                    <div key={i}>
+                        <p>{product.name}</p>
+                        <p>{product.price}</p>
+                    </div>
+                );
+            });
             return (
-                <div key={i}>
-                    <p>{product.name}</p>
-                    <p>{product.price}</p>
-                </div>
+                <>
+                    {productList}
+                    {totalCart}
+                </>
+            )
+        } else {
+            return (
+                <p> empty </p>
             );
-        });
-    } else {
-        return (
-            <p> empty </p>
-        );
+        };
     };
-}
 
 export default Cart;
