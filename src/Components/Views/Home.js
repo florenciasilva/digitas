68b91a6b4311db1faa081a2a3ff487c2c5aa62de
florenciasilva <hensgrej@gmail.com>
2020-01-19 20:52:47 -0600

    Route protection and signout btn

--- src/Components/Views/Home.js
+++ src/Components/Views/Home.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom'
+import firebase from 'firebase';
+import Redeem from '../Reedem';
+import Products from '../Products';
 
-const Home = () => (
-    <>
-        <p> home</p>
-    </>
-);
+const Home = (props) => {
+    const [ user, setUser ] = useState({});
 
-export default Home;
+    const checkUser = () => {
+        firebase.auth().onAuthStateChanged((user) => {
+          if(user == null || user == undefined) {
+            props.history.push("/");
+          } else {
+              setUser(user)
+            }
+        });
+    };
+    
+    useEffect(() => {
+        checkUser();
+    });
+    
+    const logOut = () => {
+        firebase.auth().signOut().then(() => console.log('log out'))
+        .catch(err => console.log(err))
+    }
+
+    return (
+        <>
+            <p> Bienvenido {user.email}</p>
+            <button onClick={logOut}> signOut</button>
+            <Redeem uid={user.uid}/>
+            <Products />
+        </>
+    );
+};
+
+export default withRouter(Home);
